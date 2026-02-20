fda9478152cfb187e259e174650eef36957458b5
Nguyen Anh Tuan <nganhtuan5847@gmail.com>
2026-02-20 10:50:25 +0700

    docs : xay dung cau hoi cho quiz

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,70 @@ const resultMessage = document.getElementById('result-message');
 const restartBtn = document.getElementById('restart-btn');
 // Thanh tiến trình (progress bar) hiển thị % hoàn thành bài quiz
 const progressBar = document.getElementById('progress');
+
+// tạo câu hỏi
+const quizQuestions = [
+    {
+        question: "Ai là người lãnh đạo Cách mạng Tháng Tám năm 1945?",
+        answers: [
+            { text: "Võ Nguyên Giáp", isCorrect: false },
+            { text: "Hồ Chí Minh", isCorrect: true },
+            { text: "Phan Bội Châu", isCorrect: false },
+            { text: "Ngô Đình Diệm", isCorrect: false }
+        ]
+    },
+    {
+        question: "Chiến thắng Điện Biên Phủ diễn ra vào năm nào?",
+        answers: [
+            { text: "1945", isCorrect: false },
+            { text: "1954", isCorrect: true },
+            { text: "1968", isCorrect: false },
+            { text: "1975", isCorrect: false }
+        ]
+    },
+    {
+        question: "Ai là vị tướng chỉ huy chiến dịch Điện Biên Phủ?",
+        answers: [
+            { text: "Trần Hưng Đạo", isCorrect: false },
+            { text: "Võ Nguyên Giáp", isCorrect: true },
+            { text: "Nguyễn Huệ", isCorrect: false },
+            { text: "Lê Lợi", isCorrect: false }
+        ]
+    },
+    {
+        question: "Nhà Trần đã 3 lần đánh bại quân xâm lược nào?",
+        answers: [
+            { text: "Quân Minh", isCorrect: false },
+            { text: "Quân Thanh", isCorrect: false },
+            { text: "Quân Nguyên - Mông", isCorrect: true },
+            { text: "Quân Pháp", isCorrect: false }
+        ]
+    },
+    {
+        question: "Chiến thắng Bạch Đằng năm 938 do ai lãnh đạo?",
+        answers: [
+            { text: "Ngô Quyền", isCorrect: true },
+            { text: "Lý Thường Kiệt", isCorrect: false },
+            { text: "Trần Hưng Đạo", isCorrect: false },
+            { text: "Đinh Bộ Lĩnh", isCorrect: false }
+        ]
+    },
+    {
+        question: "Ngày Giải phóng miền Nam, thống nhất đất nước là ngày nào?",
+        answers: [
+            { text: "2/9/1945", isCorrect: false },
+            { text: "30/4/1975", isCorrect: true },
+            { text: "7/5/1954", isCorrect: false },
+            { text: "19/8/1945", isCorrect: false }
+        ]
+    },
+    {
+        question: "Ai là người sáng lập ra nước Văn Lang?",
+        answers: [
+            { text: "An Dương Vương", isCorrect: false },
+            { text: "Hùng Vương", isCorrect: true },
+            { text: "Lạc Long Quân", isCorrect: false },
+            { text: "Triệu Đà", isCorrect: false }
+        ]
+    }
+];
